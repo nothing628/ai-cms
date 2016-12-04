04b93618c3d5e82316f85c730e7dedf2eb72d767
nothing628 <yogyphang@gmail.com>
2016-12-04 08:16:16 +0700

    update composer

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -12,22 +12,17 @@
 		<div id="navbar" class="navbar-collapse collapse">
 			<ul class="nav navbar-nav">
 				<li class="active"><a href="#">Home</a></li>
-				<li><a href="#about">Artist</a></li>
 				<li><a href="#about">Category</a></li>
-				<li><a href="#about">About</a></li>
-				<li><a href="#contact">Contact</a></li>
-				<!-- <li class="dropdown">
-					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button">Browse</a>
 					<ul class="dropdown-menu">
-						<li><a href="#">Action</a></li>
-						<li><a href="#">Another action</a></li>
-						<li><a href="#">Something else here</a></li>
-						<li role="separator" class="divider"></li>
-						<li class="dropdown-header">Nav header</li>
-						<li><a href="#">Separated link</a></li>
-						<li><a href="#">One more separated link</a></li>
+						<li><a href="#">Popular</a></li>
+						<li><a href="#">Recently Update</a></li>
+						<li><a href="#">Most View</a></li>
+						<li><a href="#">Random</a></li>
 					</ul>
-				</li> -->
+				</li>
+				<li><a href="#contact">Contact Us</a></li>
 			</ul>
 
 			<ul class="nav navbar-nav navbar-right">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,18 +13,8 @@ Welcome to ReadmeCom
 	</div>
 </div>
 
-<div class="row">
-	<div class="col-md-12">
-		<h3>Recently Update</h3>
-
-		<div class="row">
-			<div class="col-md-2"><img src="{{ url('images/medium/Cover 001.jpg') }}" alt="" class="img-responsive"></div>
-			<div class="col-md-2"><img src="{{ url('images/medium/Cover 002.jpg') }}" alt="" class="img-responsive"></div>
-			<div class="col-md-2"><img src="{{ url('images/medium/Cover 003.jpg') }}" alt="" class="img-responsive"></div>
-			<div class="col-md-2"><img src="{{ url('images/medium/Cover 004.jpg') }}" alt="" class="img-responsive"></div>
-			<div class="col-md-2"><img src="{{ url('images/medium/Cover 005.jpg') }}" alt="" class="img-responsive"></div>
-			<div class="col-md-2"><img src="{{ url('images/medium/Cover 006.jpg') }}" alt="" class="img-responsive"></div>
-		</div>
-	</div>
-</div>
+@include('home.recent')
+@include('home.popular')
+@include('home.viewer')
+@include('home.random')
 @endsection
