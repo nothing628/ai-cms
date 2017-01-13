6b4cde787dbd5f1f2c518df405fd8187d95469db
nothing628 <yogyphang@gmail.com>
2017-01-14 02:02:37 +0700

    add breadcrumb

--- resources/views/admin/base.blade.php
+++ resources/views/admin/base.blade.php
@@ -1,7 +1,13 @@
 @extends('layouts.admin.base')
 
 @section('content')
+	<ol class="breadcrumb">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Manga</a></li>
+	</ol>
+	<div class="clearfix"></div>
+	<div class="page-content">
 	@section('page-content')
-
 	@show
+	</div>
 @endsection
--- resources/views/admin/manga.blade.php
+++ resources/views/admin/manga.blade.php
@@ -5,5 +5,23 @@ Manga
 @endsection
 
 @section('page-content')
+<a href="#" class="btn btn-success btn-flat btn-line"><i class="fa fa-upload"></i> Upload</a>
 
+<table class="table table-bordered" style="margin-top: 15px;">
+	<thead>
+		<tr>
+			<th>#</th>
+			<th>Title</th>
+			<th>Page</th>
+			<th>Action</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td></td>
+			<td>Test</td>
+			<td>123</td>
+		</tr>
+	</tbody>
+</table>
 @endsection
