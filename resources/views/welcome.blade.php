a5e24e96ecf66f76150c93fe2975f52bb03ddd93
nothing628 <yogyphang@gmail.com>
2017-02-04 23:08:58 +0700

    change config to setting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,14 @@
 @extends ('layouts.base')
 
 @section ('title')
-Welcome to {{ Config::get('app.name') }}
+Welcome to {{ Setting::get('app.name') }}
 @endsection
 
 @section ('content')
 <div class="bg-primary-dark">
 	<section class="content content-full content-boxed overflow-hidden">
 		<div class="push-100-t push-50 text-center">
-			<h1 class="h2 text-white push-10 animated fadeInDown" data-toggle="appear" data-class="animated fadeInDown">Welcome to {{ Config::get('app.name') }}</h1>
+			<h1 class="h2 text-white push-10 animated fadeInDown" data-toggle="appear" data-class="animated fadeInDown">Welcome to {{ Setting::get('app.name') }}</h1>
 			<h2 class="h5 text-white-op animated fadeInDown" data-toggle="appear" data-class="animated fadeInDown">{{ Setting::get('app.desc') }}</h2>
 		</div>
 	</section>
