42cb82573c76212ff8c5cc116ced75115318b9a7
nothing628 <yogyphang@gmail.com>
2017-02-12 16:14:17 +0700

    migrate to component

--- resources/views/admin/chapter/create.blade.php
+++ resources/views/admin/chapter/create.blade.php
@@ -6,32 +6,37 @@
 
 @section('breadcrumb')
 <li><a>Home</a></li>
-<li><a>Manga</a></li>
-<li><a>Chapter</a></li>
-<li><a>Create New</a></li>
+<li><a class="link-effect">Manga</a></li>
+<li><a class="link-effect">Chapter</a></li>
+<li><a class="link-effect">Upload</a></li>
 @endsection
 
 @section('page-content')
-<form method="post" class="form-horizontal" enctype="multipart/form-data">
-	{!! csrf_field() !!}
-	<input type="hidden" name="manga_id" value="{{ $manga->id }}">
-	<div class="form-group">
-		<label class="control-label col-md-2">Chapter Title</label>
-		<div class="col-md-6">
-			<input type="text" name="title" class="form-control" placeholder="Chapter Title" required="">
-		</div>
-	</div>
-	<div class="form-group">
-		<label class="control-label col-md-2">Cover</label>
-		<div class="col-md-6">
-			<input type="file" name="cover" accept="image/*">
-		</div>
-	</div>
-	<div class="form-group">
-		<div class="col-md-offset-2 col-md-9">
-			<button class="btn btn-success btn-flat btn-line">Submit</button>
-			<a href="{{ route('admin.manga.chapter', $manga->id) }}" class="btn btn-danger btn-line btn-flat">Cancel</a>
-		</div>
-	</div>
-</form>
+<vue-block :is-themed="true">
+	<vue-block-head :data-class="['bg-modern-dark']">Upload New Chapter</vue-block-head>
+	<vue-block-content>
+		<form method="post" class="form-horizontal" enctype="multipart/form-data">
+			{!! csrf_field() !!}
+			<input type="hidden" name="manga_id" value="{{ $manga->id }}">
+			<vue-form-group>
+				<label class="control-label col-md-2">Chapter Title</label>
+				<div class="col-md-6">
+					<input type="text" name="title" class="form-control" placeholder="Chapter Title" required="">
+				</div>
+			</vue-form-group>
+			<vue-form-group>
+				<label class="control-label col-md-2">Cover</label>
+				<div class="col-md-6">
+					<input type="file" name="cover" accept="image/*">
+				</div>
+			</vue-form-group>
+			<vue-form-group>
+				<div class="col-md-offset-2 col-md-9">
+					<button class="btn btn-success btn-flat btn-line">Submit</button>
+					<a href="{{ route('admin.manga.chapter', $manga->id) }}" class="btn btn-danger btn-line btn-flat">Cancel</a>
+				</div>
+			</vue-form-group>
+		</form>
+	</vue-block-content>
+</vue-block>
 @endsection
