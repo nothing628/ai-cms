1a2f0bc70aa5a441017edf8ae794cb8d25125ce4
nothing628 <yogyphang@gmail.com>
2017-02-18 23:03:30 +0700

    Fix missing part upload just change config because session id always change for every request

--- config/chunk-upload.php
+++ config/chunk-upload.php
@@ -25,8 +25,8 @@ return [
 		// setup for the chunk naming setup to ensure same name upload at same time
 		"name" => [
 			"use" => [
-				"session" => true, // should the chunk name use the session id? The uploader muset send cookie!,
-				"browser" => false // instead of session we can use the ip and browser?
+				"session" => false, // should the chunk name use the session id? The uploader muset send cookie!,
+				"browser" => true // instead of session we can use the ip and browser?
 			]
 		]
 	]
