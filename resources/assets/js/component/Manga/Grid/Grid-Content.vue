4c5386e508bf56312d7051eff334fa7f080471e5
nothing628 <yogyphang@gmail.com>
2017-02-19 20:54:42 +0700

    add 5 columns grid for medium width

--- resources/assets/js/component/Manga/Grid/Grid-Content.vue
+++ resources/assets/js/component/Manga/Grid/Grid-Content.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="col-md-2 col-xl-2 col-sm-4 col-xs-6">
+	<div class="col-md-20p col-xl-2 col-sm-4 col-xs-6">
 		<div class="img-container ribbon ribbon-modern ribbon-danger ribbon-left">
 			<img :src="item.thumb_url" :alt="item.title" class="img-responsive">
 			<div class="ribbon-box">
