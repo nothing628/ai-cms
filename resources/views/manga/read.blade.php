d315f247f7c1b65e6fcaa6d067fe0dba2525e437
nothing628 <yogyphang@gmail.com>
2017-02-25 13:45:34 +0700

    read page

--- resources/views/manga/read.blade.php
+++ resources/views/manga/read.blade.php
@@ -5,10 +5,10 @@
 @endsection
 
 @section('content')
-<div class="content bg-modern-dark">
+<div class="content bg-gray-lighter">
 	<div class="row items-push">
 		<div class="col-sm-7">
-			<h1 class="page-heading text-muted">
+			<h1 class="page-heading">
 				{{ $manga->title }}
 			</h1>
 		</div>
