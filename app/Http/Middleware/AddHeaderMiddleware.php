a19456b89d56ab13cc0c2d0463132cc3abc2bf97
nothing628 <yogyphang@gmail.com>
2017-02-28 23:46:54 +0700

    modify header

--- app/Http/Middleware/AddHeaderMiddleware.php
+++ app/Http/Middleware/AddHeaderMiddleware.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use DateTime;
+use DateInterval;
 
 class AddHeaderMiddleware
 {
@@ -16,11 +17,16 @@ class AddHeaderMiddleware
 	 */
 	public function handle($request, Closure $next)
 	{
+		$days2 = new DateInterval("P2D");
+		$expire = new DateTime();
+		$expire->add($days2);
+		$max_age = $days2->d * 60 * 60 * 24;
 		$response = $next($request);
 
 		$response->headers->set('X-XSS-Protection', '1; mode=block');
+		$response->headers->set('Cache-Control', 'max-age=' . $max_age);
 		$response->setLastModified(new DateTime("now"));
-		$response->setExpires(new DateTime("tomorrow"));
+		$response->setExpires($expire);
 
 		return $response;
 	}
