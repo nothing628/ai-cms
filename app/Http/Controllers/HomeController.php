24dd446daca0f87360913bcf83aa68b13e319aa7
nothing628 <yogyphang@gmail.com>
2017-02-28 22:51:18 +0700

    change view for seo optimizing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use SEOMeta;
 use OpenGraph;
 use Twitter;
 use Setting;
+use App\Models\Manga;
 
 class HomeController extends Controller
 {
@@ -17,11 +18,19 @@ class HomeController extends Controller
 		OpenGraph::setUrl(route('home'));
 		OpenGraph::addProperty('type', 'articles');
 
+		Twitter::setTitle(Setting::get('app.title'));
+		Twitter::setDescription(Setting::get('app.description'));
+
 		SEOMeta::setTitle(Setting::get('app.title'));
 		SEOMeta::setDescription(Setting::get('app.description'));
 		SEOMeta::addMeta('robots', 'index,follow');
 		SEOMeta::addKeyword(Setting::get('app.keyword'));
-		return view('home.index');
+
+		$popular = Manga::popular()->limit(15)->get();
+		$view = Manga::mostView()->limit(15)->get();
+		$random = Manga::random()->limit(15)->get();
+
+		return view('home.index', ['popular' => $popular, 'view' => $view, 'random' => $random]);
 	}
 
 	public function search()
--- resources/views/home/popular.blade.php
+++ resources/views/home/popular.blade.php
@@ -8,13 +8,46 @@
 			</ul>
 			<vue-block-content :data-class="['remove-padding','tab-content']" id="lts-grp">
 				<div role="tabpanel" id="popular" class="tab-pane active">
-					<manga-grid data-source="{{ route('api.manga.get') }}" :data-options="{scope: 'popular'}">Most Popular Manga</manga-grid>
+					<div is="manga-grid" data-source="{{ route('api.manga.get') }}" :data-options="{scope: 'popular'}">
+						@foreach ($popular as $manga)
+						<a href="{{$manga->manga_url}}">
+							<span>Title : {{$manga->title}}</span>
+							<span>Artist : {{$manga->meta['artist']}}</span>
+							<span>Author : {{$manga->meta['author']}}</span>
+							<span>Synopsis : {{$manga->meta['desc']}}</span>
+							<span>Tag : {{$manga->tag_names}}</span>
+							<img src="{{$manga->thumb_url}}" alt="{{$manga->title}}">
+						</a>
+						@endforeach
+					</div>
 				</div>
 				<div role="tabpanel" id="view" class="tab-pane">
-					<manga-grid data-source="{{ route('api.manga.get') }}" :data-options="{scope: 'view'}">Most View Manga</manga-grid>
+					<div is="manga-grid" data-source="{{ route('api.manga.get') }}" :data-options="{scope: 'view'}">
+						@foreach ($view as $manga)
+						<a href="{{$manga->manga_url}}">
+							<span>Title : {{$manga->title}}</span>
+							<span>Artist : {{$manga->meta['artist']}}</span>
+							<span>Author : {{$manga->meta['author']}}</span>
+							<span>Synopsis : {{$manga->meta['desc']}}</span>
+							<span>Tag : {{$manga->tag_names}}</span>
+							<img src="{{$manga->thumb_url}}" alt="{{$manga->title}}">
+						</a>
+						@endforeach
+					</div>
 				</div>
 				<div role="tabpanel" id="random" class="tab-pane">
-					<manga-grid data-source="{{ route('api.manga.get') }}" :data-options="{scope: 'random'}">Random Manga</manga-grid>
+					<div is="manga-grid" data-source="{{ route('api.manga.get') }}" :data-options="{scope: 'random'}">
+						@foreach ($random as $manga)
+						<a href="{{$manga->manga_url}}">
+							<span>Title : {{$manga->title}}</span>
+							<span>Artist : {{$manga->meta['artist']}}</span>
+							<span>Author : {{$manga->meta['author']}}</span>
+							<span>Synopsis : {{$manga->meta['desc']}}</span>
+							<span>Tag : {{$manga->tag_names}}</span>
+							<img src="{{$manga->thumb_url}}" alt="{{$manga->title}}">
+						</a>
+						@endforeach
+					</div>
 				</div>
 			</vue-block-content>
 		</vue-block>
