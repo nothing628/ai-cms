71bb4457e7d38d4bbcdaafd0a4e3ab8e6f5e9e65
nothing628 <yogyphang@gmail.com>
2017-03-04 18:10:40 +0700

    fix error variable

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Category;
 
 class TagController extends Controller
 {
@@ -18,6 +19,7 @@ class TagController extends Controller
 
     public function directory()
     {
-    	return view('home.tag');
+    	$categories = Category::all();
+    	return view('home.tag', ['categories' => $categories]);
     }
 }
