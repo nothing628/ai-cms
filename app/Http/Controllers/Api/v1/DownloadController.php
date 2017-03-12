dda725a2134f6ea20a4df5a47119cbe02389e979
nothing628 <yogyphang@gmail.com>
2017-03-12 20:58:17 +0700

    add method download

--- app/Http/Controllers/Api/v1/DownloadController.php
+++ app/Http/Controllers/Api/v1/DownloadController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Api\v1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Downloader\DownloaderFacade;
 
 class DownloadController extends Controller
 {
-	//
+	public function download(Request $request)
+	{
+		$url = $request->url;
+		$dest = $request->dest;
+	}
 }
