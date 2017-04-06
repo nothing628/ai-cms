a411f4a217be640396499482b07607f5e98d6ff7
nothing628 <yogyphang@gmail.com>
2017-04-06 23:52:29 +0700

    fix typo

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -15,7 +15,7 @@
 	<div class="row items-push text-uppercase">
 		<div class="col-xs-6 col-sm-3">
 			<div class="font-w700 text-gray-darker animated fadeIn">Member for</div>
-			<a class="h2 font-w300 text-primary animated flipInX" href="javascript:void(0)">{{ $user->created_at->diffInDays($now)) }} Days</a>
+			<a class="h2 font-w300 text-primary animated flipInX" href="javascript:void(0)">{{ $user->created_at->diffInDays($now) }} Days</a>
 		</div>
 		<div class="col-xs-6 col-sm-3">
 			<div class="font-w700 text-gray-darker animated fadeIn">Manga Read</div>
